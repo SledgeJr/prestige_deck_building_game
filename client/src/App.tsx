66eb44d85210bb83139a873f9a845469a8679cdd
Max Feldhamer <max.feldhamer@gmail.com>
2025-08-14 13:33:51 +0300

    Updated card design. Added optional query param of card_type to card api. Started implementing cardHand component to store and display hand of cards.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import CardFetcher from './components/CardFetcher'
+import CardHand from './components/cardHand'
 import './App.css'
+
 import { Express } from 'express'
+import Card from 'components/card'
 
 function App() {
   const server_url = "http://localhost:4000"
   const [count, setCount] = useState(0)
 
+  const card_types = ['gold', 'knight', 'wench']
   return (
+    // <div className='hand'>
+    //   <CardHand cards={card_types}></CardHand>
+    // </div>
     <>
-      <CardFetcher></CardFetcher>
+      <CardFetcher cardType='gold'></CardFetcher>
+      <CardFetcher cardType='knight'></CardFetcher>
+      <CardFetcher cardType='wench'></CardFetcher>
     </>
   )
 }
